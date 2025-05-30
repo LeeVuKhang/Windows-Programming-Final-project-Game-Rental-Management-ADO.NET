abde17fbed8eeab19a0216b876b3536c7d752d45
LeeVuKhang <lvukhangpq@gmail.com>
2025-05-30 17:47:04 +0700

    FrmBranchRevenueReport.Chart++

--- Game Rental Management/Game Rental Management/FrmBranchRevenueReport.Designer.cs
+++ Game Rental Management/Game Rental Management/FrmBranchRevenueReport.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.dgvREVENUE = new System.Windows.Forms.DataGridView();
@@ -37,14 +40,17 @@
             this.dtpToDate = new System.Windows.Forms.DateTimePicker();
             this.dtpFromDate = new System.Windows.Forms.DateTimePicker();
             this.btnGenerate = new System.Windows.Forms.Button();
+            this.chartRevenue = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvREVENUE)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRevenue)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.chartRevenue);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.dgvREVENUE);
             this.panel1.Location = new System.Drawing.Point(14, 12);
@@ -68,7 +74,7 @@
             this.dgvREVENUE.Location = new System.Drawing.Point(14, 58);
             this.dgvREVENUE.Name = "dgvREVENUE";
             this.dgvREVENUE.RowHeadersWidth = 62;
-            this.dgvREVENUE.Size = new System.Drawing.Size(615, 321);
+            this.dgvREVENUE.Size = new System.Drawing.Size(357, 321);
             this.dgvREVENUE.TabIndex = 0;
             // 
             // panel2
@@ -88,7 +94,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(363, 29);
+            this.label6.Location = new System.Drawing.Point(337, 28);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(57, 16);
             this.label6.TabIndex = 30;
@@ -98,7 +104,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(58, 29);
+            this.label4.Location = new System.Drawing.Point(34, 28);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 16);
             this.label4.TabIndex = 29;
@@ -106,18 +112,18 @@
             // 
             // dtpToDate
             // 
-            this.dtpToDate.Location = new System.Drawing.Point(425, 29);
+            this.dtpToDate.Location = new System.Drawing.Point(399, 28);
             this.dtpToDate.Margin = new System.Windows.Forms.Padding(2);
             this.dtpToDate.Name = "dtpToDate";
-            this.dtpToDate.Size = new System.Drawing.Size(135, 20);
+            this.dtpToDate.Size = new System.Drawing.Size(194, 20);
             this.dtpToDate.TabIndex = 28;
             // 
             // dtpFromDate
             // 
-            this.dtpFromDate.Location = new System.Drawing.Point(142, 28);
+            this.dtpFromDate.Location = new System.Drawing.Point(118, 27);
             this.dtpFromDate.Margin = new System.Windows.Forms.Padding(2);
             this.dtpFromDate.Name = "dtpFromDate";
-            this.dtpFromDate.Size = new System.Drawing.Size(135, 20);
+            this.dtpFromDate.Size = new System.Drawing.Size(185, 20);
             this.dtpFromDate.TabIndex = 27;
             // 
             // btnGenerate
@@ -134,6 +140,22 @@
             this.btnGenerate.UseVisualStyleBackColor = false;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
+            // chartRevenue
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartRevenue.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartRevenue.Legends.Add(legend2);
+            this.chartRevenue.Location = new System.Drawing.Point(377, 58);
+            this.chartRevenue.Name = "chartRevenue";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartRevenue.Series.Add(series2);
+            this.chartRevenue.Size = new System.Drawing.Size(262, 321);
+            this.chartRevenue.TabIndex = 2;
+            this.chartRevenue.Text = "chart1";
+            // 
             // FrmBranchRevenueReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -147,6 +169,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvREVENUE)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRevenue)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -162,5 +185,6 @@
         private System.Windows.Forms.DateTimePicker dtpToDate;
         private System.Windows.Forms.DateTimePicker dtpFromDate;
         private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRevenue;
     }
 }
--- Game Rental Management/Game Rental Management/FrmBranchRevenueReport.cs
+++ Game Rental Management/Game Rental Management/FrmBranchRevenueReport.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
 
 namespace Game_Rental_Management
@@ -28,14 +29,56 @@ namespace Game_Rental_Management
 
         private void btnGenerate_Click(object sender, EventArgs e)
         {
+            if (dtpFromDate.Value > dtpToDate.Value)
+            {
+                MessageBox.Show("Ngày bắt đầu phải nhỏ hơn hoặc bằng ngày kết thúc.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             dtBranchRevenue = new DataTable();
             dtBranchRevenue.Clear();
-            DataSet ds = dbBranchRevenue.GetData(dtpFromDate.Value, dtpToDate.Value );
+            DataSet ds = dbBranchRevenue.GetData(dtpFromDate.Value, dtpToDate.Value);
             dtBranchRevenue = ds.Tables[0];
             dgvREVENUE.DataSource = dtBranchRevenue;
-            dgvREVENUE.Columns[0].Width = 80;  // BranchID
+
+            //DataGridView
+            dgvREVENUE.AllowUserToAddRows = false;
+            dgvREVENUE.ReadOnly = true;
+            dgvREVENUE.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvREVENUE.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvREVENUE.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvREVENUE.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgvREVENUE.Columns[0].Width = 75;   // BranchID
             dgvREVENUE.Columns[1].Width = 150;  // BranchName
-            dgvREVENUE.Columns[2].Width = 80;  // Revenue
+            dgvREVENUE.Columns[2].Width = 70;   // Revenue
+
+            //Chart 
+            chartRevenue.Series.Clear();
+            chartRevenue.ChartAreas.Clear();
+
+            ChartArea area = new ChartArea("RevenueChartArea");
+            area.AxisX.LabelStyle.Angle = -30;
+            area.AxisX.Interval = 1;
+            area.AxisX.Title = "Chi nhánh";
+            area.AxisY.Title = "Doanh thu";
+            area.AxisX.LabelStyle.Font = new Font("Segoe UI", 8, FontStyle.Regular);
+            area.AxisY.LabelStyle.Font = new Font("Segoe UI", 8, FontStyle.Regular);
+            chartRevenue.ChartAreas.Add(area);
+
+            Series series = new Series("Revenue");
+            series.ChartType = SeriesChartType.Column;
+            series.XValueMember = "BranchName";
+            series.YValueMembers = "Revenue";
+            series.Color = Color.DodgerBlue;
+            series["PixelPointWidth"] = "40";
+            series.Font = new Font("Segoe UI", 8, FontStyle.Regular);
+
+            chartRevenue.Series.Add(series);
+            chartRevenue.Legends.Clear(); 
+
+            chartRevenue.DataSource = dtBranchRevenue;
+            chartRevenue.DataBind();
         }
+
     }
 }
